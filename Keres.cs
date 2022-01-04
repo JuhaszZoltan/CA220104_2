71354c3333935d910351a9d4db54b19f835b5b03
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2022-01-04 17:49:49 +0100

    Add project files.

---
+++ Keres.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220104_2
+{
+    internal class Keres
+    {
+        public string Cim { get; set; }
+        public string DatumIdo { get; set; }
+        public string Get { get; set; }
+        public string HttpKod { get; set; }
+        private string MeretString { get; set; }
+        public int ByteMeret => 
+            MeretString == "-" 
+            ? 0 
+            : int.Parse(MeretString);
+        public bool Domain => 
+            !int.TryParse(Cim[Cim.Length - 1].ToString(), out _);
+
+        public Keres(string sor)
+        {
+            var v = sor.Split('*');
+            Cim = v[0];
+            DatumIdo = v[1];
+            Get = v[2];
+            var w = v[3].Split(' ');
+            HttpKod = w[0];
+            MeretString = w[1];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA220104_2
+{
+    internal class Program
+    {
+        static List<Keres> keresek = new List<Keres>();
+        static void Main()
+        {
+            Feladat04();
+            Feladat05();
+            Feladat06();
+            Feladat08();
+            Feladat09();
+            Console.ReadLine();
+        }
+
+        private static void Feladat04()
+        {
+            using (var sr = new StreamReader("..\\..\\res\\NASAlog.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    keresek.Add(new Keres(sr.ReadLine()));
+                }
+            }
+        }
+        private static void Feladat05()
+        {
+            Console.WriteLine($"5. feladat: Kérések száma: {keresek.Count}");
+        }
+        private static void Feladat06()
+        {
+            int sum = keresek.Sum(x => x.ByteMeret);
+            Console.WriteLine($"6. feladat: Válaszok összmérete: {sum} byte");
+        }
+        private static void Feladat08()
+        {
+            int db = keresek.Count(x => x.Domain);
+            Console.WriteLine(
+                "8. feladat: Domain-es kérések: " +
+                "{0:0.00}%", db / (float)keresek.Count * 100);
+        }
+        private static void Feladat09()
+        {
+            Console.WriteLine("9. feladat: Statisztika:");
+            var bg = keresek
+                .GroupBy(x => x.HttpKod)
+                .ToDictionary(x => x.Key, x => x.Count());
+            foreach (var kvp in bg)
+            {
+                Console.WriteLine($"\t{kvp.Key}: {kvp.Value} db");
+            }
+        }
+    }
+}
